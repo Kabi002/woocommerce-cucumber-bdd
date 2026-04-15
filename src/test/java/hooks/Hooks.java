b153361d25e1638e5c97a78386ee261cfe5b510d
Kabilan-12taste <kabilan.mohanraj@12taste.com>
2026-04-15 15:47:58 +0530

    updated hooks class

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -3,11 +3,18 @@ package hooks;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+
 import utils.DriverManager;
 import config.ConfigReader;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Hooks {
 
@@ -16,18 +23,39 @@ public class Hooks {
     @Before
     public void setUp(Scenario scenario) {
         System.out.println("Starting Scenario: " + scenario.getName());
+
         driver = DriverManager.getDriver();
         driver.get(ConfigReader.getBaseUrl());
     }
 
     @After
     public void tearDown(Scenario scenario) {
+
         if (scenario.isFailed()) {
-            byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-            scenario.attach(screenshot, "image/png", scenario.getName());
+            try {
+                byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+
+                scenario.attach(screenshot, "image/png", scenario.getName());
+
+                Files.createDirectories(Paths.get("reports/screenshots"));
+
+                String fileName = scenario.getName().replaceAll("[^a-zA-Z0-9]", "_") + ".png";
+
+                Files.write(
+                        Paths.get("reports/screenshots/" + fileName),
+                        screenshot
+                );
+
+                System.out.println("Saved screenshot: reports/screenshots/" + fileName);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+
         System.out.println("Finished Scenario: " + scenario.getName());
         System.out.println("Status: " + scenario.getStatus());
+
         DriverManager.quitDriver();
     }
 }
